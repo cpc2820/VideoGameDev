eb1ddb403f1814923666e953d7cff1f49df3cc37
Jet-Wang <jwang945@gatech.edu>
2021-03-24 13:51:21 -0400

    changed roomba ai to waypoint and implemented chasing appropriately

--- Assets/Scripts/roombaAI.cs
+++ Assets/Scripts/roombaAI.cs
@@ -6,16 +6,32 @@ public class roombaAI : MonoBehaviour
 {
     public UnityEngine.AI.NavMeshAgent agent;
     public GameObject target;
+    public Vector3 initialMovementDir;
+    public int rayCastDist = 3;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(Vector3.forward * -10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.transform.position);
-        Debug.Log("chasing target");
+
+        //have agent move in a straight line, once it hits an obstacle
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), rayCastDist))
+        {
+            Debug.Log("hit wall");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * rayCastDist, Color.yellow);
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.,Random.Range(-20.0f, 20.0f),0), Time.deltaTime * 1);
+            //agent.SetDestination(transform.TransformDirection(transform.forward));
+            agent.Move(transform.TransformDirection(Vector3.forward * -1));
+        }
+        else
+        {
+            Debug.Log("not hit wall");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+        }
     }
 }
---
+++ Assets/Scripts/roombaAIWaypoint.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class roombaAIWaypoint : MonoBehaviour
+{
+    public enum AIState
+    {
+        Patrol,
+        Chase
+    }
+    public AIState aiState;
+    public GameObject[] waypoints;
+    public int currWaypoint;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public GameObject player;
+    public float fieldOfViewAngle = 15.0f;
+    public bool showRoombaViews;
+    public int viewDistance = 20;
+    public int followTimeMax = 1000;
+    public NavMeshHit hit;
+    private int currFollowTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currWaypoint = -1;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        aiState = AIState.Patrol;
+        setNextWaypoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetDir = player.transform.position - transform.position;
+        float angle = Vector3.Angle(targetDir, transform.forward);
+        //Vector3.Angle only returns positive value, use cross product to see if needs to be negative
+        Vector3 cross = Vector3.Cross(targetDir, transform.forward);
+        if (cross.y > 0)
+        {
+            angle = -angle;
+        }
+
+        if (showRoombaViews)
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * viewDistance, Color.white); //straight
+            Debug.DrawRay(transform.position, transform.TransformDirection(Quaternion.AngleAxis(angle, Vector3.up) * Vector3.forward) * viewDistance, Color.cyan); //player
+            Debug.DrawRay(transform.position, transform.TransformDirection(Quaternion.AngleAxis(fieldOfViewAngle, Vector3.up) * Vector3.forward) * viewDistance, Color.magenta); //edge of view
+            Debug.DrawRay(transform.position, transform.TransformDirection(Quaternion.AngleAxis(-fieldOfViewAngle, Vector3.up) * Vector3.forward) * viewDistance, Color.magenta); //edge of view
+        }
+        if (angle < fieldOfViewAngle && targetDir.magnitude <= viewDistance) //player within distance and within angle
+        {
+            //now check if raycast returns player
+            if (!agent.Raycast(player.transform.position, out hit)) //agent.Raycast returns false if it has a clear line of sight between it and the player
+            {
+                aiState = AIState.Chase;
+            }
+        }
+        switch (aiState)
+        {
+            case AIState.Patrol:
+                currFollowTime = 0;
+                agent.speed = 3.5f;
+                agent.SetDestination(waypoints[currWaypoint].transform.position);
+                break;
+            case AIState.Chase:
+                agent.speed = 8.0f;
+                agent.SetDestination(player.transform.position);
+                currFollowTime++;
+                if (currFollowTime >= followTimeMax)
+                {
+                    aiState = AIState.Patrol;
+                }
+                break;
+        }
+        if (agent.remainingDistance <= 0.6 && !agent.pathPending)
+        {
+            setNextWaypoint();
+        }
+    }
+
+    private void setNextWaypoint()
+    {
+        if (waypoints.Length == 0)
+        {
+            Debug.Log("waypoints array is empty");
+        }
+        else
+        {
+            currWaypoint = (currWaypoint + 1) % waypoints.Length;
+        }
+    }
+}
