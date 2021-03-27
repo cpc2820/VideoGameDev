13de608d2ba47ebb31b8cfb94e7adcd1cb471c91
GTSuber <jsuber6@gatech.edu>
2021-03-27 12:40:28 -0400

    Update branch

---
+++ Assets/Scripts/SinkDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkDamage : MonoBehaviour
+{
+	public GameObject sink;
+	public GameObject hole2;
+	Vector3 offset = new Vector3(0, 0, 10);
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+        	
+            other.gameObject.transform.position = hole2.transform.position - offset;
+        }
+    }
+}
