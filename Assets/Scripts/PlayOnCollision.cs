ee40a49033c3470b1472432b8bf8917f5d3bf419
spadi29 <42748341+spadi29@users.noreply.github.com>
2021-04-06 10:56:44 -0400

    added collision sound script

---
+++ Assets/Scripts/PlayOnCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnCollision : MonoBehaviour
+{
+    public AudioSource collision_sound;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > 1)
+            collision_sound.Play();
+    }
+}
